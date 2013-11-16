75210e7293e7ae665c7e508b0b4fa1089f4144d0
mofengfly <wxl920@126.com>
2013-11-16 11:13:55 +0800

    Update reg.js

--- src/utils/reg.js
+++ src/utils/reg.js
@@ -1,2 +1,11 @@
 //url正则
 var urlReg=new RegExp("((^http)|(^https)|(^ftp)):\/\/(\\w)+\.(\\w)+");
+
+//简单html标签
+var RE_SIMPLE_TAG = /^<(\w+)\s*\/?>(?:<\/\1>)?$/;
+
+var R_HTML = /<|&#?\w+;/;
+//标签
+var RE_TAG = /<([\w:]+)/;
+var R_LEADING_WHITESPACE = /^\s+/;
+var R_TAIL_WHITESPACE = /\s+$/,
